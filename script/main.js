a58cce624a1f2fc6c6ae43b0f2a24f1cb7df9ffd
Aaron_V <aaronv152@gmail.com>
2023-09-18 13:01:54 -0400

    js 2.0

--- script/main.js
+++ script/main.js
@@ -3,13 +3,28 @@ const but_Med = document.getElementById("click_Med");
 const but_Ava = document.getElementById("click_Ava");
 const detDiv = document.querySelector("#ver_detalles");
 const presentacion = document.querySelector(".presentacion");
-but_Bas.addEventListener("click", mostrarTar)
-presentacion.addEventListener("click", ()=>{
+but_Bas.addEventListener("click", ()=>{
+    detDiv.classList.remove("none");
+    agregarImg(but_Bas);
+})
+but_Med.addEventListener("click", ()=>{
+    detDiv.classList.remove("none");
+    agregarImg(but_Med);
+})
+but_Ava.addEventListener("click", ()=>{
+    detDiv.classList.remove("none");
+    agregarImg(but_Ava);
+})
+presentacion.addEventListener("dblclick", ()=>{
     detDiv.classList.add("none");
 })
-function mostrarTar (){
-    detDiv.classList.remove("none");
-}
+// function agregarImg (boton){
+    
+// } 
+// function mostrarTar (){
+//     // console.log(boton);
+//     detDiv.classList.remove("none");
+// }
 let fig = document.querySelectorAll(".previewImg figure");
 let preview = document.querySelector(".selectedPreview figure");
 for (const figures of fig){
